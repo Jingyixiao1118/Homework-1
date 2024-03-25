e096a87550dd4dea81046c46ad56600b77814910
Jingyixiao1118 <160853244+Jingyixiao1118@users.noreply.github.com>
2024-03-25 14:00:02 +0800

    Update Classroom.sol

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -3,10 +3,21 @@ pragma solidity ^0.8.0;
 
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
-    // Note: You can declare some state variable
+    uint256 private _registrationCode;
+    bool private _firstRegisterCall = true; 
+
+    constructor() {
+        _registrationCode = 1234;
+    }
 
     function register() external returns (uint256) {
-        // TODO: please add your implementaiton here
+        if (_firstRegisterCall) {
+            _firstRegisterCall = false; 
+        } else {
+            _registrationCode = 123;
+        }
+        
+        return _registrationCode;
     }
 }
 
